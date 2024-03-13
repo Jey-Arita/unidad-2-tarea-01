da8808cc4d11d1a0f52d27d792510369dd53dbf5
Jey-Arita <jomaarita@gmail.com>
2024-03-12 19:11:03 -0600

    ejersicio 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('Hola');
+let num1 = parseInt(prompt('Ingrese primer numero'));
+let num2 = parseInt(prompt('Ingrese segundo numero'));
+
+if(num1 > num2){
+    console.log(`El numero ${num1} es mayor que ${num2}`);
+}else if(num1 === num2){
+    console.log(`El numero ${num2} es igual que ${num1}`);
+}else{
+    console.log(`El numero ${num2} es mayor que ${num1}`);
+}
